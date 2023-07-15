abd221e828f23b44f05a4b6bf18665b80c2a7750
Ryan <124264948+narevox@users.noreply.github.com>
2023-07-15 15:30:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get('/bard', async (req, res) => {
     	console.log(response)
         res.send({status: 200, bardcookies: bardcookies, response: response});
 });
-app.listen(8000)
+app.listen(5000)
 }
 
 lol()
