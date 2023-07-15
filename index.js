6e0aa816be7f7e8844c38f7331e2b53e67a2ac0d
Ryan <124264948+narevox@users.noreply.github.com>
2023-07-15 15:04:33 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+import express from 'express'
+import { Bard } from "googlebard";
+const app = express()
+async function lol () {
+app.get('/', function (req, res) {
+    res.send("hello world")
+})
+
+app.get('/bard', async (req, res) => {
+    const text = req.query.text;
+    const id = req.query.id || "id";
+    const bardcookies = req.query.bardcookies;
+    
+    if (!text) return res.send("put some text dumb ass")
+    if (!bardcookies) return res.send("put some bardcookies dumb ass")
+    
+    	let cookies = `__Secure-1PSID=`+bardcookies;
+		let bot = new Bard(cookies, {
+  			  inMemory: false,
+  			  savePath: "./conversations.json", // this is being done to save crucial information about the conversation so the bot remembers it
+		});
+		console.log(text, id, bardcookies)
+		let conversationId = id;
+		let response = await bot.ask(text, conversationId);
+    	console.log(response)
+        res.send({status: 200, bardcookies: bardcookies, response: response});
+});
+app.listen(8443)
+}
+
+lol()
